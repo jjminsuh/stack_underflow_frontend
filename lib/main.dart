4277bd4ea8b9856adfdfc34e0867c6e29055f61e
AlwaysFighting <87655596+AlwaysFighting@users.noreply.github.com>
2023-02-11 21:27:23 +0900

    err: 구글맵 애뮬레이터 오류

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:stack_underflow_frontend/map/map_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -10,6 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: _title,
