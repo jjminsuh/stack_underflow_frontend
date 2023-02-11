5746c9f4727614463f5a073080d490dcc846ed56
jiyun B <jiyunbae36@gmail.com>
2023-02-11 23:50:29 +0900

    feat: add ocr feature

---
+++ android/app/src/main/kotlin/com/example/cureapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cureapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Utils/image_picker_class.dart
@@ -0,0 +1,22 @@
+import 'package:image_picker/image_picker.dart';
+import 'dart:developer';
+
+Future<String> pickImage({ImageSource? source}) async {
+  final picker = ImagePicker();
+
+  String path = '';
+
+  try {
+    final getImage = await picker.pickImage(source: source!);
+
+    if (getImage != null) {
+      path = getImage.path;
+    } else {
+      path = '';
+    }
+  } catch (e) {
+    log(e.toString());
+  }
+
+  return path;
+}
