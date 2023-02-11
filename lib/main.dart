6f14276f9ff15a58e65663be23220e58174ff241
adityasundar15 <adityasundar15@gmail.com>
2023-02-12 03:19:05 +0900

    Completed code for translate and OCR. Just install Flask and googletrans (specific version) and start the flask server before starting flutter app

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'dart:developer';
+
+import 'package:cureapp/Utils/image_cropper_page.dart';
+import 'package:cureapp/Utils/image_picker_class.dart';
+import 'package:cureapp/Widgets/modal_dialog.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'Screen/recognization_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          imagePickerModal(context, onCameraTap: () {
+            log("Camera");
+            pickImage(source: ImageSource.camera).then((value) {
+              if (value != '') {
+                imageCropperView(value, context).then((value) {
+                  if (value != '') {
+                    Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                        builder: (_) => RecognizePage(
+                          path: value,
+                        ),
+                      ),
+                    );
+                  }
+                });
+              }
+            });
+          }, onGalleryTap: () {
+            log("Gallery");
+            pickImage(source: ImageSource.gallery).then((value) {
+              if (value != '') {
+                imageCropperView(value, context).then((value) {
+                  if (value != '') {
+                    Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                        builder: (_) => RecognizePage(
+                          path: value,
+                        ),
+                      ),
+                    );
+                  }
+                });
+              }
+            });
+          });
+        },
+        tooltip: 'Increment',
+        label: const Text("Scan Photo"),
+      ),
+    );
+  }
+}
